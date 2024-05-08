1918c7daf472a763af8d3055a3132d73501bf656
Brenton Carter <99493283+Inserian@users.noreply.github.com>
2024-05-07 23:35:16 -0800

    Create Requirements.bat

---
+++ Requirements.bat
@@ -0,0 +1,20 @@
+@echo off
+cls
+echo Checking for Python installation...
+where python > nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Python is not installed. Please install Python and try again.
+    pause
+    exit /b
+)
+
+echo Python is installed.
+echo Setting up the environment...
+
+python -m pip install PyQt5 numpy pyaudio --user
+
+echo Starting the application...
+start "" "Sonic.exe"
+echo Exiting setup.
+pause
+exit
